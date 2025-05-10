9e00618b8e1d118892f94d8d34a4e5d3ee34ea91
Santiago <santtyconla@gmail.com>
2025-05-10 10:35:23 -0500

    Agregar archivos de proyecto.

---
+++ SEBDOM_SAS/Migrations/20250510151009_modify4.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SEBDOM_SAS.Migrations
+{
+    /// <inheritdoc />
+    public partial class modify4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
